e32a42900a8c6eba33f5e8953aa8e7870e907144
JerukPurut404 <114576248+JerukPurut404@users.noreply.github.com>
2022-11-04 22:31:38 +0100

    API url changed
    and some changes around certain feature

--- wallpaper.py
+++ wallpaper.py
@@ -2,33 +2,42 @@ import requests
 import platform
 import os
 import winreg, ctypes, win32con
-
+from PIL import Image
+from time import sleep 
+import random
 FILL, FIT, STRETCH, TILE, CENTER, SPAN = 0, 1, 2, 3, 4, 5
 MODES = (0, 10), (0, 6), (0, 2), (1, 0), (0, 0), (0, 22)
-value1, value2 = MODES[STRETCH] 
 
-url = "https://pic.re/images"
-
-key = winreg.OpenKey(
-	winreg.HKEY_CURRENT_USER, r"Control Panel\Desktop", 0, winreg.KEY_WRITE
-)
-winreg.SetValueEx(key, "TileWallpaper", 0, winreg.REG_SZ, str(value1))
-winreg.SetValueEx(key, "WallpaperStyle", 0, winreg.REG_SZ, str(value2))
-winreg.CloseKey(key)
-
+url = "https://wallhaven.cc/api/v1/search?categories=010&sorting=random&resolutions=1920x1080"
+answer_yes = ["Y", "y", "yes"]
+answer_no = ["N", "n", "no"]
 
 def download_pic_of_day():
-	r = requests.get(url, allow_redirects=True)
-	if r.status_code != 200:
-		print("error")
-		return
+	data = requests.get(url).json()
+	response = data['data'][int(random.randint(1,24))]['path']
+	image_data = requests.get(response).content
 	filename = "images.jpg"
-	open(filename, "wb").write(r.content)
+	open(filename, "wb").write(image_data)
+	with Image.open(os.path.abspath(filename)) as img:
+		width, height = img.size
+		print("The picture size is " + str(img.size))
+	if img.size[0] > 1080:
+		value1, value2 = MODES[FILL]
+	else:
+		value1, value2 = MODES[STRETCH]
+	key = winreg.OpenKey(
+	winreg.HKEY_CURRENT_USER, r"Control Panel\Desktop", 0, winreg.KEY_WRITE
+	)
+	winreg.SetValueEx(key, "TileWallpaper", 0, winreg.REG_SZ, str(value1))
+	winreg.SetValueEx(key, "WallpaperStyle", 0, winreg.REG_SZ, str(value2))
+	winreg.CloseKey(key)
 	print(f"saved the picture to {filename}!")
+	sleep(5)
+		
 
 def download_again():
 	repeat = input("Would you like to download another pic? Y or N? ")
-	if repeat == "Y":
+	if repeat in answer_yes:
 		download_pic_of_day()
 		changed = win32con.SPIF_UPDATEINIFILE | win32con.SPIF_SENDCHANGE
 		cmd = ctypes.windll.user32.SystemParametersInfoW(
@@ -38,7 +47,7 @@ def download_again():
 		os.system("rundll32.exe user32.dll, UpdatePerUserSystemParameters")
 		os.system("cls")
 		download_again()
-	elif repeat == "N":
+	elif repeat in answer_no:
 		quit()
 	else:
 		os.system("cls")
@@ -54,6 +63,3 @@ if __name__ == "__main__":
 	os.system("rundll32.exe user32.dll, UpdatePerUserSystemParameters")
 	os.system("cls")
 	download_again()
-
-
-
